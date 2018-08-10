3540b8f1a32b71d08a7ca250cfe20f976a06c4a8
Andrew Nowak <andrew@nowakfamily.net>
2018-08-10 17:56:16 -0400

    initial commit

---
+++ src/main/java/com/auction/andrew/auction/AuctionApplication.java
@@ -0,0 +1,30 @@
+package com.auction.andrew.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class AuctionApplication {
+
+	@RequestMapping("/auctionItems/")
+	String getAuctionItems() {
+		return "{[{\"auctionItemId\": \"1234\"},{\"auctionItemId\": \"5678\"}]}";
+	}
+
+	@RequestMapping("/auctionItems/{auctionItemId}")
+	String getAuctionItem(String auctionItemId) {
+		//Todo: Learn how to pass in parameters.
+		return "{\"auctionItemId\": \"" + auctionItemId + "\"}";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApplication.class, args);
+	}
+}
