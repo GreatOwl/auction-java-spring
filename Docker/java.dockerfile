6c8aefa00fb808fff4fb899b152f682fe0302d9f
Andrew Nowak <andrew@nowakfamily.net>
2018-08-11 19:03:34 -0400

    allow containerization of components

---
+++ Docker/java.dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk
+
+RUN apt-get update
+RUN apt-get -y install maven
+
+
--- src/main/java/com/auction/andrew/auction/AuctionApplication.java
+++ src/main/java/com/auction/andrew/auction/AuctionApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.*;
 
 @SpringBootApplication
 @RestController
